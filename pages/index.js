b9efd163838237939cf0d0beb4e4a17a90fe99b1
Nijin <nijin.vinodan@siriuscom.com>
2020-08-16 23:26:29 +0530

    Added an Index Page

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const Index = () => {
+    return (
+        <div>
+            <h2>JSON MOCK DATA</h2>
+            <ul>
+                <li><a href="/api/users">Users</a></li>
+                <li><a href="/api/blogs">Blogs</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Index;
